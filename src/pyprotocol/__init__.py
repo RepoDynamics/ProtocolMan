d678dad02e697d846f461b051f6d03f8f5219c17
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-21 13:42:53 +0200

    update

--- src/pyprotocol/__init__.py
+++ src/pyprotocol/__init__.py
@@ -1,11 +1,12 @@
 """Commonly used protocols for type hinting."""
 
-from typing import Protocol as _Protocol
+from typing import Protocol as _Protocol, runtime_checkable as _runtime_checkable
 
 
+@_runtime_checkable
 class Stringable(_Protocol):
     """An object that can be converted to a string,
-    i.e., has a `__str__` method."""
+    i.e., one that has a `__str__` method."""
 
     def __str__(self) -> str:
         ...
